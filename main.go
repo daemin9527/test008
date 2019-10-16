ef9a1d33453f4cb4d7af0ac9a0b1196976c42ac9
Damien <249016681@qq.com>
2019-10-16 20:23:17 +0800

    Update main.go
    s

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		log.Printf("title: %s\n", title)
 	}
 
-	fmt.Fprintf(w, "Hello fr1om:  "+title+"\n")
+	fmt.Fprintf(w, "Hello fr1om:  "+tit1le+"\n")
 }
 
 func main() {
